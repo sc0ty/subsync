ec121d6c3e26be28ad4590b8ab32efebe262f148
Michał Szymaniak <sc0typl@gmail.com>
2019-07-09 21:58:46 +0200

    default caption for PopupInfoButton

--- subsync/gui/components/popups.py
+++ subsync/gui/components/popups.py
@@ -12,6 +12,6 @@ class PopupInfoButton(wx.BitmapButton):
     def onClick(self, event):
         if self.message:
             parent = self.GetParent()
-            caption = self.GetLabel()
+            caption = self.GetLabel() or _('Info')
             with wx.MessageDialog(parent, self.message, caption) as dlg:
                 dlg.ShowModal()
