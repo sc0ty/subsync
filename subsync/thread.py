9b4cde588d63ff9ea69f4f656682008f055e2529
Michał Szymaniak <sc0typl@gmail.com>
2019-11-22 19:17:37 +0100

    AsyncJob private members prefixed with underscore

--- subsync/thread.py
+++ subsync/thread.py
@@ -37,46 +37,46 @@ class AtomicInt(AtomicValue):
 
 class AsyncJob(object):
     def __init__(self, job, name=None):
-        self.loop = None
-        self.task = None
-        self.thread = None
-        self.name = name
-        self.job = job
+        self._loop = None
+        self._task = None
+        self._thread = None
+        self._name = name
+        self._job = job
 
     def start(self, *args, **kwargs):
         thread = threading.Thread(
-                name=self.name,
-                args=[self.job] + list(args),
+                name=self._name,
+                args=[self._job] + list(args),
                 kwargs=kwargs,
                 target=self._run)
 
         thread.start()
-        self.thread = thread
+        self._thread = thread
 
     def startSynchronous(self, *args, **kwargs):
-        self._run(self.job, *args, **kwargs)
+        self._run(self._job, *args, **kwargs)
 
     def stop(self):
-        if self.loop:
-            self.loop.call_soon_threadsafe(self.task.cancel)
-        if self.thread and self.thread.isAlive():
-            self.thread.join()
+        if self._loop:
+            self._loop.call_soon_threadsafe(self._task.cancel)
+        if self._thread and self._thread.isAlive():
+            self._thread.join()
 
     def isRunning(self):
-        return self.thread and self.thread.isAlive()
+        return self._thread and self._thread.isAlive()
 
     def getResult(self):
-        if self.task:
+        if self._task:
             try:
-                return self.task.result()
+                return self._task.result()
             except asyncio.CancelledError or asyncio.InvalidStateError:
                 pass
 
     def _run(self, job, *args, **kwargs):
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.loop)
-        self.task = asyncio.ensure_future(job(*args, **kwargs))
+        self._loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self._loop)
+        self._task = asyncio.ensure_future(job(*args, **kwargs))
         try:
-            self.loop.run_until_complete(self.task)
+            self._loop.run_until_complete(self._task)
         except asyncio.CancelledError:
             pass
